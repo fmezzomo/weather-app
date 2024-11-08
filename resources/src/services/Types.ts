6885a4f7fb399363b18a2be74d2c2b77bdd0225e
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-11-08 10:52:22 -0300

    Created types file

---
+++ resources/src/services/Types.ts
@@ -0,0 +1,28 @@
+export interface City {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    sys: {
+      country: string;
+    };
+  }
+  
+  export interface WeatherData {
+    city: {
+      name: string;
+      country: string;
+    };
+    list: Array<{
+      dt: number;
+      main: {
+        temp: number;
+      };
+      weather: Array<{
+        description: string;
+      }>;
+    }>;
+  }
+  
